389c91a33f1bb08e48df82741939c7a368dbea85
ameliameyer <amelianmeyer@gmail.com>
2026-07-08 20:58:49 -0700

    Update in_practice_particles.js

--- js/in_practice_particles.js
+++ js/in_practice_particles.js
@@ -1,4 +1,7 @@
 particlesJS.load(
   'particles-js',
-  '../in_practice_particles.json'
+  '../in_practice_particles.json',
+  function () {
+    console.log('in practice particles loaded');
+  }
 );
